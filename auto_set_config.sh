79f4fdad3728ef3d6a405dac7cc3f19457cf8cbf
zhk-0315 <zhk_0315@163.com>
2021-01-09 22:28:48 +0000

    qa

--- auto_set_config.sh
+++ auto_set_config.sh
@@ -2,5 +2,5 @@
 touch ~/.xprofile
 echo -e "tint2 &\nnm-applet &\n\nexport LANG=zh_CN.UTF-8\nexport LANGUAGE=zh_CN:en_US" >> ~/.xprofile
 cp /etc/bash.bashrc ~/.bashrc
-sudo pacman -S bash-completion
-echo -e "export PATH=$PATH:/usr/share\nexport LANG=zh_CN.UTF-8\nexport LANGUAGE=zh_CN:en_US" >> ~/.bashrc
+sudo pacman -S bash-completion neofetch lolcat
+echo -e "export PATH=$PATH:/usr/share\nexport LANG=zh_CN.UTF-8\nexport LANGUAGE=zh_CN:en_US\nneofetch | lolcat\nlspci | grep -i vga" >> ~/.bashrc
