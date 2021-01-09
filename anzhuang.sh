51cccaa23581c5bfb38123347a79520b84dcab4b
zhk-0315 <zhk_0315@163.com>
2021-01-09 16:34:43 +0800

    s

--- anzhuang.sh
+++ anzhuang.sh
@@ -13,12 +13,19 @@ locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 hostnamectl set-hostname arch
 echo -e "127.0.0.1  localhost\n::1      localhost\n127.0.1.1    arch.localdomain    arch" >> /etc/hosts
-echo "\n\n\n\n\n\n" | pacman -S grub efibootmgr networkmanager network-manager-applet dialog wireless_tools wpa_supplicant os-prober mtools dosfstools ntfs-3g base-devel linux-lts-headers reflector git sudo
+echo '\n\n\n\n\n\n' | pacman -S grub efibootmgr networkmanager network-manager-applet dialog wireless_tools wpa_supplicant os-prober mtools dosfstools ntfs-3g base-devel linux-lts-headers reflector git sudo
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Archlinux
 mkdir /mnt/win
 read -p "mount win sysdisk": windisk
-mount $windisk /mnt/win
+if [ $windisk == /nothing ];then
+else	
+    mount $windisk /mnt/win
+fi
 grub-mkconfig -o /boot/grub/grub.cfg
 echo '\n\n\n\n\n' | pacman -S xorg-server xorg-xinit xorg-apps lightdm lightdm-gtk-greeter xfce4 xfce4-goodies ttf-sarasa-gothic nvidia nvidia-utils
 systemctl enable NetworkManager 
 systemctl enable lightdm
+passwd
+useradd -mG wheel zhang
+passwd zhang
+EDITOR=nano visudo
