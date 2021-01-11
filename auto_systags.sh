1750990ed09375a278db0909effcf7e5ff3646e1
zhk-0315 <zhk_0315@163.com>
2021-01-11 10:41:28 +0800

    a

--- auto_systags.sh
+++ auto_systags.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
-if [ ! -f $HOME/.cache/systags ];then
-    ctags -I __THROW --extras=+F --langmap=c:+.h --languages=c,c++ --links=yes --c-kinds=+p \
-        --fields=+S  -R -f $HOME/.cache/systags /usr/include /usr/local/include
-fi
+ctags -I __THROW --extras=+F --langmap=c:+.h --languages=c,c++ --links=yes --c-kinds=+p \
+    --fields=+S  -R -f $HOME/.cache/systags /usr/include /usr/local/include
